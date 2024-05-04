b41d33627d98007d3d447d4c6402497f62f10c57
JhonyGEM <jbulpas@etsinf.upv.es>
2024-05-04 18:02:45 +0200

    Modificaciones
    Modificado el fxml y el launcher para ajustar parametros de visualizacion. Añadida una imagen para visualizar. Modificada la distribucion de archivos para una mejor organizacion. Editada la hoja de estilos solo para pruebas

--- IPC-Cuenta_de_gastos/src/javafxmlapplication/JavaFXMLApplication.java
+++ IPC-Cuenta_de_gastos/src/javafxmlapplication/JavaFXMLApplication.java
@@ -13,24 +13,35 @@ import javafx.stage.Stage;
 
 
 public class JavaFXMLApplication extends Application {
+
+    // Minimo de redimension de la pantalla (X)
+    private static final int MIN_WIDTH = 640;
+    // Minimo de redimension de la pantalla (Y) 
+    private static final int MIN_HEIGHT = 480;
        
-    // Modificar si se cambia el nombre del archivo
-    private final String archivo = "BaseInicial_BETA";
+    // Nombre de la ruta del archivo FXML
+    private final String archivo = "/fxmls/BaseInicial_BETA";
     
     @Override
     public void start(Stage stage) throws Exception {
-        //======================================================================
+
+        // Establecido minimo posible de redimension de pantalla (ACTUALMENTE VGA)
+        stage.setMinWidth(MIN_WIDTH);
+        stage.setMinHeight(MIN_HEIGHT);
+        
         // Modificado el archivo FXML para que concuerde con el nombre
         FXMLLoader loader= new  FXMLLoader(getClass().getResource(archivo + ".fxml"));
+
         Parent root = loader.load();
-        //======================================================================
-        // 2- creación de la escena con el nodo raiz del grafo de escena
+
+        // Creación de la escena con el nodo raiz del grafo de escena
         Scene scene = new Scene(root);
-        //======================================================================
-        // 3- asiganación de la escena al Stage que recibe el metodo 
+
+        // Asiganación de la escena al Stage que recibe el metodo 
         //     - configuracion del stage
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
+
         // Modificado para que cambie el titulo de arriba (antes ponia proyecto inicial)
         stage.setTitle("IPC-ENTREGA");
         stage.show();
