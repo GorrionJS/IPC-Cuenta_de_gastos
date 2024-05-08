c2efb31f4bc307211a6cf585cdc40faf07fa6ad0
jonathan9406 <jonathanrichard9406@gmail.com>
2024-05-08 23:50:02 +0200

    se ha añadido los fxml de register y añadir gasto, aún falta implementar funcionalidad
    nuevos FXML

--- IPC-Cuenta_de_gastos/src/controlers/BaseInicial_BETAController.java
+++ IPC-Cuenta_de_gastos/src/controlers/BaseInicial_BETAController.java
@@ -2,14 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package javafxmlapplication;
+package controlers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -24,6 +30,10 @@ public class BaseInicial_BETAController implements Initializable {
     @FXML
     // Boton de Registrarse
     private Button singup_button;
+    @FXML
+    private BorderPane borderPANE;
+    @FXML
+    private VBox border;
 
     /**
      * Initializes the controller class.
@@ -33,12 +43,20 @@ public class BaseInicial_BETAController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void login(ActionEvent event) {
-    }
+
 
     @FXML
     private void signup(ActionEvent event) {
     }
+
+    @FXML
+    private void login(MouseEvent event) throws IOException {
+//        System.out.println("hola");
+//        FXMLLoader fxmlLogin = new FXMLLoader(getClass().getResource("formulario.fxml"));
+//        Parent root = fxmlLogin.load();
+//        border.setDisable(true);
+//        border.setClip(root);
+    }
+    
     
 }
---
+++ IPC-Cuenta_de_gastos/src/controlers/FormularioController.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controlers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class formularioController implements Initializable {
+
+    @FXML
+    private TextField userText;
+    @FXML
+    private Text wrongUserText;
+    @FXML
+    private TextField paswordText;
+    @FXML
+    private Text wrongPassText;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cancelarM(MouseEvent event) {
+    }
+
+    @FXML
+    private void aceptarM(MouseEvent event) {
+    }
+    
+}
