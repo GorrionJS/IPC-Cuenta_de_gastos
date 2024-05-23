7452c909a432c02b327324a50a0219ba2b386967
JhonyGEM <jbulpas@etsinf.upv.es>
2024-05-23 14:16:16 +0200

    ?

--- IPC-Cuenta_de_gastos/src/controlers/MiPerfilController.java
+++ IPC-Cuenta_de_gastos/src/controlers/MiPerfilController.java
@@ -27,12 +27,7 @@ import model.AcountDAOException;
  */
 public class MiPerfilController implements Initializable {
     
-    private static final String INICIO = "/fxmls/";
-    private static final String PERFIL = "/fxmls/";
-    private static final String GASTOS = "/fxmls/misGastos";
-    private static final String AYUDA = "/fxmls/";
-    private static final String EXPORTAR = "/fxmls/Exportar";
-    private static final String ANTERIOR = "/fxmls/Marco_Vacio_Inicial";
+
 
     @FXML
     private BorderPane borderPANE;
@@ -44,16 +39,6 @@ public class MiPerfilController implements Initializable {
     private ImageView userProfile;
     @FXML
     private Label userName;
-    @FXML
-    private Button inicioButton;
-    @FXML
-    private Button profileButton;
-    @FXML
-    private Button gastosButton;
-    @FXML
-    private Button exportarButton;
-    @FXML
-    private Button signOutButton;
     
     private Acount cuenta;
     
@@ -66,86 +51,6 @@ public class MiPerfilController implements Initializable {
         
         
     }    
-    
-
-    /**************************************************************************
-     *                          BOTON INCIO
-     *
-     */
-    
-    @FXML
-    private void inicio(ActionEvent event) throws IOException {
-        FXMLLoader inicio = new FXMLLoader(getClass().getResource(INICIO + ".fxml"));
-        AnchorPane pane = inicio.load();
-        
-    }
-    /**************************************************************************
-     *                          BOTON PERFIL
-     *
-     */
-
-    @FXML
-    private void miPerfil(ActionEvent event) {
-    }
-
-    /**************************************************************************
-     *                          BOTON GASTOS
-     *
-     */
-    
-    @FXML
-    private void gastos(ActionEvent event) throws IOException, AcountDAOException {
-        FXMLLoader verGasto = new FXMLLoader(getClass().getResource(GASTOS + ".fxml"));
-        AnchorPane root = verGasto.load();
-        MisGastosController control = verGasto.getController();
-        control.init(principal);
-        control.initMiperfil(this);
-        
-        borderPANE.setCenter(root);
-    }
-    
-    /**************************************************************************
-     *                          BOTON EXPORTAR
-     *
-     */
-
-    @FXML
-    private void exportar(ActionEvent event) throws IOException {
-        FXMLLoader fxml = new FXMLLoader(getClass().getResource(EXPORTAR + ".fxml"));
-        AnchorPane root = fxml.load();
-        ExportController controller = fxml.getController();
-        controller.init(cuenta, this);
-        
-        screen.getChildren().setAll(root);
-    }
-    
-    /**************************************************************************
-     *                          BOTON CERRAR SESION
-     *
-     */
-
-    @FXML
-    private void signOut(ActionEvent event) throws IOException {
-        FXMLLoader fxmlMain = new FXMLLoader(getClass().getResource(ANTERIOR + ".fxml"));
-        Parent root = fxmlMain.load();
-            
-        PrimeraPantallaController controller = fxmlMain.getController();
-        cuenta.logOutUser();
-        controller.setAcount(this.cuenta);
-            
-        BorderPane p = principal.getGrid();
-        p.getChildren().setAll(root);
-    }
-    
-    /**************************************************************************
-     *                          OTRAS CLASES
-     *
-     */
-    
-    public BorderPane getBorderPaneMiPerfilController(){
-        return borderPANE;
-    }
-    
     // METODO DE INICIALIZACION
     public void init (PrimeraPantallaController prin, Acount a){
         principal = prin;
@@ -153,4 +58,7 @@ public class MiPerfilController implements Initializable {
         userName.setText(cuenta.getLoggedUser().getNickName());
         userProfile.setImage(cuenta.getLoggedUser().getImage());
     }
+    
+    public Acount getAcount() { return cuenta; }
+    
 }
